d7092854bd72cdcf18080594387ec7448bdb3fcd
Szilágyi Krisztián <sz.krisz20010615@gmail.com>
2023-11-30 12:52:37 +0100

    Upgraded the project into .net 8 and removed the tests. Also i renamed the original project into SnakeGameCore because its no longer a console application but a library which contains the game logic.

---
+++ SnakeGameConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace SnakeGameConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        float t = 0.0f;
+        const float dt = 0.01f; // 60 FPS (1 / 60 = 0.01666666)
+
+        float currentTime = Stopwatch.GetTimestamp() / Stopwatch.Frequency;
+        float accumulator = 0.0f;
+
+        while (true)
+        {
+            float newTime = Stopwatch.GetTimestamp() / Stopwatch.Frequency;
+            float frameTime = newTime - currentTime;
+            currentTime = newTime;
+
+            accumulator += frameTime;
+
+            while (accumulator >= dt)
+            {
+                //integrate(state, t, dt);
+                accumulator -= dt;
+                t += dt;
+            }
+
+            //render(state);
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
